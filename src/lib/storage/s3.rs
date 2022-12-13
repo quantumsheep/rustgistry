f12444454f895bb0ef0a0711efd5e53e98719c45
quantumsheep <quantumsheep@users.noreply.github.com>
2022-12-14 00:14:12 +0100

    feat: implement missing s3 storage methods

--- src/lib/storage/s3.rs
+++ src/lib/storage/s3.rs
@@ -1,18 +1,21 @@
-use std::{pin::Pin, time::SystemTime};
+use std::{path::PathBuf, pin::Pin, time::SystemTime};
 
 use async_trait::async_trait;
 use bytes::Bytes;
 use futures::{Stream, StreamExt};
 use rusoto_core::{Region, RusotoError};
 use rusoto_s3::{
-    GetObjectError, GetObjectRequest, HeadObjectError, HeadObjectRequest, PutObjectRequest,
-    S3Client, StreamingBody, S3,
+    CopyObjectRequest, DeleteObjectRequest, GetObjectError, GetObjectRequest, HeadObjectError,
+    HeadObjectRequest, PutObjectRequest, S3Client, StreamingBody, S3,
 };
 use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
 use sync_wrapper::SyncWrapper;
 use tokio::io::AsyncWriteExt;
 use tokio_util::codec;
 use uuid::Uuid;
+
+use crate::utils;
 
 use super::{
     base::{ImageLayerInfo, Result, Storage, UploadContainer},
@@ -39,6 +42,33 @@ impl S3Storage {
             client,
         }
     }
+
+    fn get_upload_file_path(&self, name: &String, uuid: &String) -> String {
+        ["uploads", name, uuid]
+            .iter()
+            .collect::<PathBuf>()
+            .to_str()
+            .unwrap()
+            .to_owned()
+    }
+
+    fn get_layer_file_path(&self, name: &String, digest: &String) -> String {
+        ["layers", name, digest]
+            .iter()
+            .collect::<PathBuf>()
+            .to_str()
+            .unwrap()
+            .to_owned()
+    }
+
+    fn get_manifest_file_path(&self, name: &String, reference: &String) -> String {
+        ["manifests", name, reference]
+            .iter()
+            .collect::<PathBuf>()
+            .to_str()
+            .unwrap()
+            .to_owned()
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -55,15 +85,16 @@ impl Storage for S3Storage {
         name: String,
         digest: String,
     ) -> Result<Option<ImageLayerInfo>> {
-        let key = format!("layers/{}/{}", name, digest);
+        let key = self.get_layer_file_path(&name, &digest);
 
-        let request = GetObjectRequest {
-            bucket: self.bucket.clone(),
-            key: key.clone(),
-            ..Default::default()
-        };
-
-        let result = self.client.get_object(request).await;
+        let result = self
+            .client
+            .get_object(GetObjectRequest {
+                bucket: self.bucket.clone(),
+                key: key.clone(),
+                ..Default::default()
+            })
+            .await;
         let result = match result {
             Ok(output) => output,
             Err(e) => {
@@ -85,15 +116,16 @@ impl Storage for S3Storage {
         name: String,
         digest: String,
     ) -> Result<Pin<Box<dyn Stream<Item = Result<Bytes>> + Send>>> {
-        let key = format!("layers/{}/{}", name, digest);
+        let key = self.get_layer_file_path(&name, &digest);
 
-        let request = GetObjectRequest {
-            bucket: self.bucket.clone(),
-            key: key.clone(),
-            ..Default::default()
-        };
-
-        let result = self.client.get_object(request).await;
+        let result = self
+            .client
+            .get_object(GetObjectRequest {
+                bucket: self.bucket.clone(),
+                key: key.clone(),
+                ..Default::default()
+            })
+            .await;
         let result = match result {
             Ok(output) => output,
             Err(RusotoError::Service(GetObjectError::NoSuchKey(_))) => {
@@ -119,25 +151,27 @@ impl Storage for S3Storage {
             .unwrap()
             .as_secs();
 
-        let key = format!("uploads/{}/{}", name, uuid);
+        let key = self.get_upload_file_path(&name, &uuid);
+
+        match self
+            .client
+            .put_object(PutObjectRequest {
+                bucket: self.bucket.clone(),
+                key: key.clone(),
+                body: None,
+                ..Default::default()
+            })
+            .await
+        {
+            Ok(_) => (),
+            Err(e) => return Err(Box::new(e)),
+        }
 
         let state = UploadState {
             name: name.clone(),
             uuid: uuid.clone(),
             created_at,
         };
-
-        let request = PutObjectRequest {
-            bucket: self.bucket.clone(),
-            key: key.clone(),
-            body: None,
-            ..Default::default()
-        };
-
-        match self.client.put_object(request).await {
-            Ok(_) => (),
-            Err(e) => return Err(Box::new(e)),
-        }
 
         let state_json = serde_json::to_string(&state)?;
         Ok(UploadContainer {
@@ -147,15 +181,17 @@ impl Storage for S3Storage {
     }
 
     async fn check_upload_container_validity(&self, name: String, uuid: String) -> Result<bool> {
-        let key = format!("uploads/{}/{}", name, uuid);
+        let key = self.get_upload_file_path(&name, &uuid);
 
-        let request = HeadObjectRequest {
-            bucket: self.bucket.clone(),
-            key: key.clone(),
-            ..Default::default()
-        };
-
-        match self.client.head_object(request).await {
+        match self
+            .client
+            .head_object(HeadObjectRequest {
+                bucket: self.bucket.clone(),
+                key: key.clone(),
+                ..Default::default()
+            })
+            .await
+        {
             Ok(_) => Ok(true),
             Err(RusotoError::Service(HeadObjectError::NoSuchKey(_))) => Ok(false),
             Err(e) => Err(Box::new(e)),
@@ -169,7 +205,7 @@ impl Storage for S3Storage {
         mut stream: SyncWrapper<Pin<Box<dyn Stream<Item = Result<Bytes>> + Send>>>,
         _range: (u64, u64),
     ) -> Result<UploadStatus> {
-        let key = format!("uploads/{}/{}", name, uuid);
+        let key = self.get_upload_file_path(&name, &uuid);
 
         let tmp_file = tempfile::NamedTempFile::new()?;
 
@@ -187,14 +223,14 @@ impl Storage for S3Storage {
                 Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, e)),
             });
 
-        let request = PutObjectRequest {
-            bucket: self.bucket.clone(),
-            key: key.clone(),
-            body: Some(StreamingBody::new(byte_stream)),
-            ..Default::default()
-        };
-
-        self.client.put_object(request).await?;
+        self.client
+            .put_object(PutObjectRequest {
+                bucket: self.bucket.clone(),
+                key: key.clone(),
+                body: Some(StreamingBody::new(byte_stream)),
+                ..Default::default()
+            })
+            .await?;
         tmp_file.close()?;
 
         let request = HeadObjectRequest {
@@ -209,32 +245,169 @@ impl Storage for S3Storage {
         })
     }
 
-    async fn close_upload_container(&self, _name: String, _uuidd: String) -> Result<UploadDetails> {
-        todo!()
+    async fn close_upload_container(&self, name: String, uuid: String) -> Result<UploadDetails> {
+        let key = self.get_upload_file_path(&name, &uuid);
+
+        let result = self
+            .client
+            .get_object(GetObjectRequest {
+                bucket: self.bucket.clone(),
+                key: key.clone(),
+                ..Default::default()
+            })
+            .await?;
+
+        let mut hasher = Sha256::new();
+
+        let mut stream = result
+            .body
+            .ok_or_else(|| Error::from("Missing body in response"))?;
+        while let Some(chunk) = stream.next().await {
+            let bytes = chunk?;
+            hasher.update(&bytes);
+        }
+
+        let hash = hex::encode(hasher.finalize());
+        let digest = format!("sha256:{}", hash);
+
+        let layer_key = self.get_layer_file_path(&name, &digest);
+
+        self.client
+            .copy_object(CopyObjectRequest {
+                bucket: self.bucket.clone(),
+                copy_source: format!("{}/{}", self.bucket, key),
+                key: layer_key.clone(),
+                ..Default::default()
+            })
+            .await?;
+
+        self.client
+            .delete_object(DeleteObjectRequest {
+                bucket: self.bucket.clone(),
+                key: key.clone(),
+                ..Default::default()
+            })
+            .await?;
+
+        Ok(UploadDetails { digest })
     }
 
     async fn get_manifest_summary(
         &self,
-        _name: String,
-        _reference: String,
+        name: String,
+        reference: String,
     ) -> Result<ManifestSummary> {
-        todo!()
+        let key = self.get_manifest_file_path(&name, &reference);
+
+        let result = self
+            .client
+            .get_object(GetObjectRequest {
+                bucket: self.bucket.clone(),
+                key: key.clone(),
+                ..Default::default()
+            })
+            .await?;
+
+        let mut stream = result
+            .body
+            .ok_or_else(|| Error::from("Missing body in response"))?;
+
+        let mut manifest_content = String::new();
+        while let Some(chunk) = stream.next().await {
+            let bytes = chunk?;
+            manifest_content.push_str(&String::from_utf8(bytes.to_vec())?);
+        }
+
+        let mut hasher = Sha256::new();
+        hasher.update(&manifest_content);
+        let hash = hex::encode(hasher.finalize());
+        let digest = format!("sha256:{}", hash);
+
+        let size = result.content_length.unwrap_or(0) as u64;
+
+        Ok(ManifestSummary { digest, size })
     }
 
-    async fn get_manifest(&self, _name: String, _reference: String) -> Result<ManifestDetails> {
-        todo!()
+    async fn get_manifest(&self, name: String, reference: String) -> Result<ManifestDetails> {
+        let key = self.get_manifest_file_path(&name, &reference);
+
+        let result = self
+            .client
+            .get_object(GetObjectRequest {
+                bucket: self.bucket.clone(),
+                key: key.clone(),
+                ..Default::default()
+            })
+            .await?;
+
+        let mut stream = result
+            .body
+            .ok_or_else(|| Error::from("Missing body in response"))?;
+
+        let mut manifest_content = String::new();
+        while let Some(chunk) = stream.next().await {
+            let bytes = chunk?;
+            manifest_content.push_str(&String::from_utf8(bytes.to_vec())?);
+        }
+
+        let manifest: Manifest = serde_json::from_str(&manifest_content)?;
+
+        let mut hasher = Sha256::new();
+        hasher.update(&manifest_content);
+        let hash = hex::encode(hasher.finalize());
+        let digest = format!("sha256:{}", hash);
+
+        Ok(ManifestDetails { manifest, digest })
     }
 
     async fn update_manifest(
         &self,
-        _name: String,
-        _reference: String,
-        _manifest: Manifest,
+        name: String,
+        reference: String,
+        manifest: Manifest,
     ) -> Result<UpdateManifestDetails> {
-        todo!()
+        let json = utils::to_json_normalized(&manifest)?;
+
+        let mut hasher = Sha256::new();
+        hasher.update(&json.as_bytes());
+        let hash = hex::encode(hasher.finalize());
+        let digest = format!("sha256:{}", hash);
+
+        let key = self.get_manifest_file_path(&name, &reference);
+
+        // fs::write(&path, &json)?;
+        self.client
+            .put_object(PutObjectRequest {
+                bucket: self.bucket.clone(),
+                key: key.clone(),
+                body: Some(json.into_bytes().into()),
+                ..Default::default()
+            })
+            .await?;
+
+        self.client
+            .copy_object(CopyObjectRequest {
+                bucket: self.bucket.clone(),
+                copy_source: format!("{}/{}", self.bucket, key),
+                key: key.clone(),
+                ..Default::default()
+            })
+            .await?;
+
+        Ok(UpdateManifestDetails { digest })
     }
 
-    async fn delete_manifest(&self, _name: String, _reference: String) -> Result<()> {
-        todo!()
+    async fn delete_manifest(&self, name: String, reference: String) -> Result<()> {
+        let key = self.get_manifest_file_path(&name, &reference);
+
+        self.client
+            .delete_object(DeleteObjectRequest {
+                bucket: self.bucket.clone(),
+                key: key.clone(),
+                ..Default::default()
+            })
+            .await?;
+
+        Ok(())
     }
 }
